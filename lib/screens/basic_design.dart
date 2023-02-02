d98c947a2b12d139c0a17ceb853c700fd22b9633
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-02 13:39:27 -0300

    part 2

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -5,9 +5,41 @@ class BasicDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+    return Scaffold(
+      body: Column(
+        children: const [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          Title(),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text("Oeschinen Lake Campground",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text("Kandersteg, Switzerland",
+                  style: TextStyle(color: Colors.black38)),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
       ),
     );
   }
