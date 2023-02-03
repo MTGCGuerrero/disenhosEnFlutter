81218f018073d043abbaa902a8888e8202355928
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-03 16:31:04 -0300

    part2

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -3,42 +3,54 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
 
-
 class ScrollScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           //Background image
-            const Background(),
+          const Background(),
 
-            // Main content
-            MainContent(),
-
-
-
-
+          // Main content
+          MainContent(),
         ],
-     ),
-   );
+      ),
+    );
   }
 }
 
 class MainContent extends StatelessWidget {
- 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-      const Text("Hola Mundo"),
-      const Text("Hola Mundo"),
-      const Text("Hola Mundo"),
-      ]
+    final textStyle = TextStyle(
+        fontSize: 60, fontWeight: FontWeight.bold, color: Colors.white);
+    return SafeArea(
+      bottom: false,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "11",
+              style: textStyle,
+            ),
+            Text(
+              "Miercoles",
+              style: textStyle,
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              size: 100,
+              color: Colors.white,
+            ),
+          ]),
     );
   }
 }
@@ -51,9 +63,9 @@ class Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xff30BAd6),
-      height: double.infinity,
-      alignment: Alignment.topCenter,
-      child: const Image(image: AssetImage('assets/scroll-1.png')));
+        color: const Color(0xff30BAd6),
+        height: double.infinity,
+        alignment: Alignment.topCenter,
+        child: const Image(image: AssetImage('assets/scroll-1.png')));
   }
 }
